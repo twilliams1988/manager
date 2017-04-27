7b801672ab1a9adfa0bfae2d20198d2568f7c847
Tommy Williams <tom@teow.co.uk>
2017-04-27 17:52:57 +0100

    user auth

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  
+  componentWillMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyAvCjdymdouUCEjevYZ6nwo2kgerPLhw1U',
+      authDomain: 'manager-a6942.firebaseapp.com',
+      databaseURL: 'https://manager-a6942.firebaseio.com',
+      projectId: 'manager-a6942',
+      storageBucket: 'manager-a6942.appspot.com',
+      messagingSenderId: '617503106914'
+    };
+
+firebase.initializeApp(config);
+  }
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
     return (
-      <Provider store={createStore(reducers)}>
-        <View>
-          <Text>
-            Hello!
-          </Text>
-        </View>
+      <Provider store={store}>
+        <LoginForm />
       </Provider>
     );
   }
